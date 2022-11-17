420c4f625b2a21ba6df02dade716d9398593456a
JuanviDev <juanvidev1@gmail.com>
2022-11-17 18:03:52 -0500

    Lesson 1 - Extract Search Button

---
+++ src/SearchButton.js
@@ -0,0 +1,15 @@
+const SearchButton = () => {
+  return (
+    <div className="search-row">
+      <input
+        type="text"
+        id="customerName"
+        className="form-control"
+        placeholder="Customer name"
+      />
+      <button className="btn btn-primary">Search</button>
+    </div>
+  );
+};
+
+export default SearchButton;
